6faa7b5ddcfce3568e175bd522253d9ffca82b71
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-15 19:54:39 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+import HomeDetails from './components/HomeDetails/HomeDetails/HomeDetails';
+import Login from './components/Login/Login';
+import CreateAccount from './components/CreateAccount/CreateAccount';
+import BookingList from './components/BookingList/BookingList/BookingList';
+import AddRentHouse from './components/AddRentHouse/AddRentHouse/AddRentHouse';
+import MyRent from './components/MyRent/MyRent/MyRent';
 function App() {
   return (
-    <h1>home</h1>
+    <Router>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/home-details/:id" component={HomeDetails} />
+        <Route path="/login" component={Login} />
+        <Route path="/create-account" component={CreateAccount} />
+        <Route path="/booking-list" component={BookingList} />
+        <Route path="/add-rent" component={AddRentHouse} />
+        <Route path="/my-rent" component={MyRent} />
+
+      </Switch>
+    </Router >
   );
 }
 
