4cfeb1c41c7c4b6ff4068ded3b68c5550e778be1
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-15 19:40:33 +0600

    all page files added

---
+++ src/components/AddRentHouse/AddRentHouse/AddRentHouse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddRentHouse = () => {
+    return (
+        <div>
+            <h1>Add Rent House</h1>
+        </div>
+    );
+};
+
+export default AddRentHouse;
---
+++ src/components/HomeDetails/HomeDetails/HomeDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeDetails = () => {
+    return (
+        <div>
+            <h1>Home Details</h1>
+        </div>
+    );
+};
+
+export default HomeDetails;
