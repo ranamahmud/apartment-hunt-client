1f038266337231e310ae20e04431e2a4a07c768d
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-15 21:02:56 +0600

    header skeleton added

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#home">About</Nav.Link>
+                    <Nav.Link href="#home">Service</Nav.Link>
+                    <Nav.Link href="#home">Concerns</Nav.Link>
+                    <Nav.Link href="#home">Event</Nav.Link>
+                    <Nav.Link href="#home">Contact</Nav.Link>
+                    <Link className="nav-link text-white" to="/login">
+                        <Button variant="dark" id="login-btn">Login</Button>
+                    </Link>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Header;
