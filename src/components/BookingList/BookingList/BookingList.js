5758ac6355d47d4c64c7921bbed88c24b5a3bc81
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-16 18:47:45 +0600

    header and sidebar done

--- src/components/BookingList/BookingList/BookingList.js
+++ src/components/BookingList/BookingList/BookingList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import AdminHeader from '../../common/AdminHeader/AdminHeader';
+import AdminSidebar from '../../common/AdminSidebar/AdminSidebar';
 
 const BookingList = () => {
     return (
         <div>
             <AdminHeader title="Booking List" />
+            <div className="container-fluid row">
+
+                <AdminSidebar />
+            </div>
         </div>
     );
 };
--- src/components/common/AdminSidebar/AdminSidebar.js
+++ src/components/common/AdminSidebar/AdminSidebar.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const AdminSidebar = () => {
+
+    const isAdmin = true;
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{ height: "50vh" }}>
+            <ul id="sidebar-list" className="list-unstyled">
+
+
+
+
+                {
+
+
+                    <div>
+                        <li>
+                            <Link to="/booking-list" >
+                                <Image src={require('../../../images/icons/booking-list.png').default} /><span>Booking List</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/add-rent" >
+                                <Image src={require('../../../images/icons/plus 1.png').default} /><span>Add House</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/review" >
+                                <Image src={require('../../../images/icons/home.png').default} /> <span>My Rent</span>
+                            </Link>
+                        </li>
+
+                    </div>
+                }
+            </ul>
         </div>
     );
 };
