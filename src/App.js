a4dc99f2c4773cf9e7776cb0a62bee334a4f30b7
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-16 19:17:33 +0600

    table in booking list added

--- src/App.js
+++ src/App.js
@@ -15,21 +15,29 @@ import CreateAccount from './components/CreateAccount/CreateAccount';
 import BookingList from './components/BookingList/BookingList/BookingList';
 import AddRentHouse from './components/AddRentHouse/AddRentHouse/AddRentHouse';
 import MyRent from './components/MyRent/MyRent/MyRent';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/home-details/:id" component={HomeDetails} />
-        <Route path="/login" component={Login} />
-        <Route path="/create-account" component={CreateAccount} />
-        <Route path="/booking-list" component={BookingList} />
-        <Route path="/add-rent" component={AddRentHouse} />
-        <Route path="/my-rent" component={MyRent} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/home-details/:id" component={HomeDetails} />
+          <Route path="/login" component={Login} />
+          <Route path="/create-account" component={CreateAccount} />
+          <Route path="/booking-list" component={BookingList} />
+          <Route path="/add-rent" component={AddRentHouse} />
+          <Route path="/my-rent" component={MyRent} />
 
-      </Switch>
-    </Router >
+        </Switch>
+      </Router >
+    </UserContext.Provider>
   );
 }
 
